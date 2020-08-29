ef9138fae3495c429869ec19973ff414b45fc301
Lee Seungmin <lee@seungmin.io>
2020-08-30 02:58:42 +0900

    feat: place ExhibitionList inside each Section

--- app/screens/Home/Home.tsx
+++ app/screens/Home/Home.tsx
@@ -3,8 +3,9 @@ import styled from 'styled-components/native';
 
 import Header from './Header';
 import Carousel from './Carousel';
-import Divider from '../../components/Divider';
 import Section from './Section';
+import ExhibitionList from './ExhibitionList';
+import Divider from '../../components/Divider';
 import ScrollContainer from '../../components/ContentWrapper/ScrollContainer';
 
 export default () => {
@@ -12,11 +13,17 @@ export default () => {
     <ScrollContainer>
       <Header />
       <Carousel />
-      <Section name="회원님을 위한 맞춤 전시회"></Section>
+      <Section name="회원님을 위한 맞춤 전시회">
+        <ExhibitionList />
+      </Section>
       <Divider />
-      <Section name="이달의 인기 작품"></Section>
+      <Section name="이달의 인기 작품">
+        <ExhibitionList />
+      </Section>
       <Divider />
-      <Section name="태그별 전시회"></Section>
+      <Section name="태그별 전시회">
+        <ExhibitionList />
+      </Section>
       <Divider />
       <Section name="요즘 뜨는 아티스트"></Section>
       <Section name="이 아티스트는 어때요?"></Section>
