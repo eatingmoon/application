83302f06b00abbb487d87e49bc54281fbfcfcc70
Lee Seungmin <lee@seungmin.io>
2020-08-30 02:36:03 +0900

    feat: implement Carousel

---
+++ app/screens/Home/Carousel.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+import { screenWidth } from '../../utils/screenSize';
+
+import stoolImage from '../../assets/stool.png';
+
+export default () => {
+  return (
+    <Container>
+      <AbsoluteImage
+        source={stoolImage}
+      />
+      <Text>
+        널리와 함께,{'\n'}
+        당신의 작품을 널리.
+      </Text>
+    </Container>
+  );
+};
+
+const Container = styled.View`
+  width: ${screenWidth * 0.85}px;
+  height: 233px;
+  position: relative;
+  background-color: #7a5cc5;
+  border-radius: 15px;
+`;
+
+const AbsoluteImage = styled.Image`
+  position: absolute;
+  top: 10.2px;
+  right: -20px;
+`;
+
+const Text = styled.Text`
+  position: absolute;
+  left: 30px;
+  bottom: 29px;
+`;
--- app/screens/Home/Home.tsx
+++ app/screens/Home/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from 'styled-components/native';
 
 import Header from './Header';
+import Carousel from './Carousel';
 import ScrollContainer from '../../components/ContentWrapper/ScrollContainer';
 
 export default () => {
   return (
     <ScrollContainer>
       <Header />
+      <Carousel />
     </ScrollContainer>
   );
 };
