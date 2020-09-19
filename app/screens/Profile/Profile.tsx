ae0c147c721bec28bdc2ea363e6f2ba035781159
Lee Seungmin <lee@seungmin.io>
2020-09-19 21:25:38 +0900

    feat: style Profile component for ProfileScreen

--- app/screens/Profile/Profile.tsx
+++ app/screens/Profile/Profile.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
+import { screenWidth } from '../../utils/screenSize';
+
 import placeholderNyom from '../../assets/profiles/nyom.png';
 
 const Profile = () => {
@@ -8,7 +10,7 @@ const Profile = () => {
     <Container>
       <Information>
         <HelloText>
-          김세령 님,{'\n'}
+          <NameText>김세령</NameText> 님,{'\n'}
           안녕하세요!
         </HelloText>
         <ButtonWithTouchableOpacity>
@@ -27,19 +29,49 @@ const Profile = () => {
 export default Profile;
 
 const Container = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+  width: ${screenWidth * 0.9}px;
+  margin-bottom: 45px;
 `;
 
 const Information = styled.View`
+  margin-left: ${screenWidth * 0.05}px;
 `;
 
 const HelloText = styled.Text`
+  font-size: 30px;
+  line-height: 36px;
+  margin-bottom: 12px;
+`;
+
+const NameText = styled.Text`
+  font-weight: bold;
 `;
 
 const ButtonWithTouchableOpacity = styled.TouchableOpacity`
+  background-color: #7A5CC5;
+  padding: 11px 30px;
+  align-items: center;
+  align-self: flex-start;
+  justify-content: center;
+  border-radius: 15px;
 `;
 
 const ButtonText = styled.Text`
+  font-size: 15px;
+  line-height: 18px;
+  font-weight: bold;
+  color: white;
+  align-self: flex-start;
 `;
 
-const ProfileImage = styled.Image`
+const ProfileImage = styled.Image.attrs({
+  resizeMode: 'cover',
+})`
+  width: 136px;
+  height: 136px;
+  margin-bottom: 8px;
+  margin-right: ${screenWidth * 0.025}px;
 `;
