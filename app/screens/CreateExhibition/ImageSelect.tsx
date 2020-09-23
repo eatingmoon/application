5fdc9da72abde28a9e0561ae2db85a0ae8fc31bd
Lee Seungmin <lee@seungmin.io>
2020-09-24 05:58:39 +0900

    feat: implement ImageSelect

--- app/screens/CreateExhibition/ImageSelect.tsx
+++ app/screens/CreateExhibition/ImageSelect.tsx
@@ -1,20 +1,55 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { PermissionsAndroid } from 'react-native';
+import RNFS from 'react-native-fs';
 import PhotoSelector, { PhotoProps } from 'react-native-photo-selector';
 import styled from 'styled-components/native';
 
 interface IImageSelect {}
 
 const ImageSelect: React.FC<IImageSelect> = () => {
-  const onSelectPhoto = (images: PhotoProps[], image: PhotoProps) => {
+  const [photoSelectorReady, setPhotoSelectorReady] = useState(0);
+  const bump = () => setPhotoSelectorReady(photoSelectorReady + 1);
+
+  useEffect(() => {
+    async function hasAndroidPermission(): Promise<boolean> {
+      const permission = PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE;
+      const hasPermission = await PermissionsAndroid.check(permission);
+
+      if (!hasPermission) {
+        const status = await PermissionsAndroid.request(permission);
+        return status === 'granted';
+      }
+
+      return true;
+    }
+
+    hasAndroidPermission()
+      .then((status) => {
+        if (status) {
+          bump();
+        }
+      });
+  }, []);
+
+  const [image, setImage] = useState<any>({});
+
+  const onSelectPhoto = async (images: PhotoProps[], receivedImage: PhotoProps) => {
     console.log('selected images:', images);
-    console.log('current image:', image);
+    console.log('current image:', receivedImage);
+    const uri = receivedImage.uri;
+    setImage({ uri })
   };
 
   return (
     <Container>
-      <PhotoSelector
-        callback={onSelectPhoto}
+      <Image
+        source={image}
       />
+      {!!photoSelectorReady && (
+        <PhotoSelector
+          callback={onSelectPhoto}
+        />
+      )}
     </Container>
   );
 };
@@ -24,4 +59,9 @@ export default ImageSelect;
 const Container = styled.View`
   width: 100%;
   align-items: center;
+`;
+
+const Image = styled.Image`
+  width: 128px;
+  height: 128px;
 `;
